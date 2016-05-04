2d0dc8aa439167efc0ce6dcc184857eff3dfff50
Kieran <kier.k@hotmail.co.uk>
2016-05-04 09:00:43 +0100

    new

--- app/src/main/java/com/example/id2013_03/startProject/Tabbed_Pages/Tab_Vehicle_Spec.java
+++ app/src/main/java/com/example/id2013_03/startProject/Tabbed_Pages/Tab_Vehicle_Spec.java
@@ -4,16 +4,11 @@ package com.example.id2013_03.startProject.Tabbed_Pages;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 
 
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.GestureDetectorCompat;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,35 +25,12 @@ public class Tab_Vehicle_Spec extends Fragment{
 
         // Placing the layout into the rootView variable
         // This is also what places and fills the container on the page
-        View rootView = inflater.inflate(R.layout.test, container, false);
+        View rootView = inflater.inflate(R.layout.tabbed_vehicle_specs_main, container, false);
 
         final VerticalViewPager viewPager = (VerticalViewPager) rootView.findViewById(R.id.viewPagerVertTest);
         viewPager.setAdapter(new CustomAdapter(getActivity().getSupportFragmentManager(), getActivity()));
 
 
-
-
-        TabLayout testTab = (TabLayout) rootView.findViewById(R.id.tabLayoutTest);
-
-
-        testTab.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-
-            }
-        });
 
         return rootView;
     }
@@ -68,7 +40,7 @@ public class Tab_Vehicle_Spec extends Fragment{
         // Creating a private string that is only for this class
         // This string stores the names of each of the tabs that I have named.
         // These tab names are then stored within the variable fragments
-        private String fragments[] = {"Yo", "Test"};
+        private String fragments[] = {"Tab_Vehicle_Spec_Page1", "Test"};
 
 
         public CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext){
@@ -81,10 +53,10 @@ public class Tab_Vehicle_Spec extends Fragment{
             Fragment fragment = null;
             switch (position){
                 case 0:
-                    fragment = new Yo();
+                    fragment = new Tab_Vehicle_Spec_Page1();
                     break;
                 case 1:
-                    fragment = new YoYo();
+                    fragment = new Tab_Vehicle_Spec_Page2();
                     break;
 
             }
